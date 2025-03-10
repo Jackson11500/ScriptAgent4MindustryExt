ca7216f18de98422842ce46891fdb306525f0956
Lucky Clover <jmj18795967847@gmail.com>
2025-03-10 14:06:27 +0800

    why not

--- scripts/mapScript/floodv2/mapFilter.kts
+++ scripts/mapScript/floodv2/mapFilter.kts
@@ -2,21 +2,8 @@
 
 package mapScript.floodV2
 
-import coreLibrary.lib.with
-import coreMindustry.lib.broadcast
 import mapScript.floodV2.lib.FloodUtil
 import wayzer.MapChangeEvent
-import wayzer.MapManager
-
-listenTo<MapChangeEvent>(Event.Priority.Intercept) {
-    if (rules.defaultTeam == FloodUtil.creepTeam) {
-        broadcast("[red]禁止玩家队伍作为洪水队伍".with())
-        cancelled = true
-        launch {
-            MapManager.loadMap()
-        }
-    }
-}
 
 listenTo<MapChangeEvent>(Event.Priority.Watch) {
     if (rules.tags.containsKey(FloodUtil.TAG)) {
